e162120715e92cf454eb6467919e72a054e9d301
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 16:10:28 +0100

    Trying to sort SW

--- docs/sw.js
+++ docs/sw.js
@@ -1,51 +1,14 @@
-// load workbox
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// output successful message
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`)
-} else {
-  console.log(`Boo! Workbox didn't load 😬`)
-}
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
 
-workbox.core.setCacheNameDetails({
-  prefix: 'cld',
-  suffix: 'v1.0',
-  precache: 'precache',
-  runtime: 'runtime'
-})
-
-workbox.precaching.precacheAndRoute([
-  // precached file list
-])
-
-// Serve all html files with StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  /\.html$/,
-  new workbox.strategies.NetworkFirst()
-)
-
-// Serve all css files with StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  /\js\*.js$/,
-  new workbox.strategies.StaleWhileRevalidate()
-)
-
-// Serve all css files with StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  /\css\*.css$/,
-  new workbox.strategies.StaleWhileRevalidate()
-)
-
-// Serve all other assets with CacheFirst strategy
-workbox.routing.registerRoute(
-  /\.(?:png|jpg|jpeg|svg|gif|webp|ico|webmanifest|eot,ttf,woff,woff2)$/,
-  new workbox.strategies.CacheFirst({
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
     plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      })
-    ]
+      new CacheableResponsePlugin({ statuses: [0, 200] })
+    ],
   })
-)
+);
