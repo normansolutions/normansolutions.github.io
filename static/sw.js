051cc95c91fc6ac0b5148b8b57bc965ccd16d05c
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 21:44:08 +0100

    Trying to sort SW

--- static/sw.js
+++ static/sw.js
@@ -7,7 +7,7 @@ const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute } = workbox.precaching;
 const { setCacheNameDetails } = workbox.core;
 
-const version = "ns25";
+const version = "ns26";
 
 workbox.core.setCacheNameDetails({
   suffix: version
