d174700053baba9dfcf2f11821ffeecc74eab3b6
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 16:04:10 +0100

    Trying to sort CORS

--- static/sw.js
+++ static/sw.js
@@ -1,35 +1,51 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// load workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
-const { StaleWhileRevalidate } = workbox.strategies.StaleWhileRevalidate;
-const { CacheableResponse } = workbox.cacheableResponse;
+// output successful message
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`)
+} else {
+  console.log(`Boo! Workbox didn't load 😬`)
+}
 
+workbox.core.setCacheNameDetails({
+  prefix: 'cld',
+  suffix: 'v1.0',
+  precache: 'precache',
+  runtime: 'runtime'
+})
 
-registerRoute(
-  // Cache style resources, i.e. CSS files.
-  ({ request }) => request.destination === 'style',
-  // Use cache but update in the background.
-  new StaleWhileRevalidate({
-    // Use a custom cache name.
-    cacheName: 'css-cache',
-  })
-);
+workbox.precaching.precacheAndRoute([
+  // precached file list
+])
 
-registerRoute(
-  // Cache image files.
-  ({ request }) => request.destination === 'image',
-  // Use the cache if it's available.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'image-cache',
+// Serve all html files with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(
+  /\.html$/,
+  new workbox.strategies.NetworkFirst()
+)
+
+// Serve all css files with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
+// Serve all css files with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
+// Serve all other assets with CacheFirst strategy
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif|webp|ico|webmanifest|eot,ttf,woff,woff2)$/,
+  new workbox.strategies.CacheFirst({
     plugins: [
-      new ExpirationPlugin({
-        // Cache only 20 images.
+      new workbox.expiration.Plugin({
         maxEntries: 20,
-        // Cache for a maximum of a week.
-        maxAgeSeconds: 7 * 24 * 60 * 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60
       })
-    ],
+    ]
   })
-);
+)
