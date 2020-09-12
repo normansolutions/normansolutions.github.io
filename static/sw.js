a46ccc28177ce1e0eaa33ade0223196710c91fdc
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 15:38:08 +0100

    Trying to sort CORS

--- static/sw.js
+++ static/sw.js
@@ -1,120 +1,33 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const version = "ns28042020V4";
-const precacheCacheName = workbox.core.cacheNames.precache;
-const runtimeCacheName = workbox.core.cacheNames.runtime;
-
-
-//clear invalid caches
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches
-      .keys()
-      .then(keys => keys.filter(key => !key.endsWith(version)))
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-  );
-});
-
-
 import { registerRoute } from 'workbox-routing';
-import { NetworkFirst } from 'workbox-strategies';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 registerRoute(
-  ({ request }) => request.destination === 'script',
-  new NetworkFirst()
-);
-
-workbox.core.setCacheNameDetails({
-  prefix: "ns-ooo",
-  suffix: version,
-  precache: "precache",
-  runtime: "runtime"
-});
-
-self.__precacheManifest = [
-  {
-    url: "/img/offline.png",
-    revision: "336689360776536478"
-  },
-  {
-    url: "/offline.html",
-    revision: "336689360776536478"
-  },
-  {
-    url: "/manifest.json",
-    revision: "336689360776536478"
-  }
-].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-//cache fonts
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "ns-google-fonts-stylesheets-" + version
+  // Cache style resources, i.e. CSS files.
+  ({ request }) => request.destination === 'style',
+  // Use cache but update in the background.
+  new StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
   })
 );
 
-workbox.routing.registerRoute(
-  /.*(?:googleapis)\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "ns-googleapis-" + version
-  })
-);
-
-workbox.routing.registerRoute(
-  /.*(?:gstatic)\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "ns-gstatic-" + version
-  })
-);
-
-workbox.routing.registerRoute(
-  /\**\*.(?:css)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "ns-static-css" + version
-  })
-);
-
-workbox.routing.registerRoute(
-  /\**\*.(?:js)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "ns-static-js" + version
-  })
-);
-
-//cache all images with limits
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: "ns-images-" + version,
+registerRoute(
+  // Cache image files.
+  ({ request }) => request.destination === 'image',
+  // Use the cache if it's available.
+  new CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
     plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 250,
-        maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-        purgeOnQuotaError: true
+      new ExpirationPlugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
       })
-    ]
+    ],
   })
 );
-
-//cache gallery with refresh
-workbox.routing.registerRoute(
-  /.*/,
-  workbox.strategies.cacheFirst({
-    cacheName: "ns-main-" + version,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-        purgeOnQuotaError: true
-      })
-    ]
-  })
-);
-
-//offline fallback
-const FALLBACK_URL = "/Methods/offline";
-const urlHandler = workbox.strategies.cacheFirst();
-workbox.routing.registerRoute(new RegExp("/.*"), ({ event }) => {
-  return urlHandler.handle({ event }).catch(() => caches.match(FALLBACK_URL));
-});
