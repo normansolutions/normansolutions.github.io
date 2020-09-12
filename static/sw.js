faa0aaaadb5d02fdf38de4074bcd12a5c031db3a
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 16:02:41 +0100

    Trying to sort CORS

--- static/sw.js
+++ static/sw.js
@@ -2,6 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const { registerRoute } = workbox.routing;
 const { CacheFirst } = workbox.strategies;
+const { StaleWhileRevalidate } = workbox.strategies.StaleWhileRevalidate;
 const { CacheableResponse } = workbox.cacheableResponse;
 
 
@@ -9,7 +10,7 @@ registerRoute(
   // Cache style resources, i.e. CSS files.
   ({ request }) => request.destination === 'style',
   // Use cache but update in the background.
-  workbox.strategies.staleWhileRevalidate({
+  new StaleWhileRevalidate({
     // Use a custom cache name.
     cacheName: 'css-cache',
   })
