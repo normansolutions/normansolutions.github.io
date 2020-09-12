4b1ecfeb5528f60e942c862782fde2d059c9b82b
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 22:50:11 +0100

    Trying to sort SW

--- docs/sw.js
+++ docs/sw.js
@@ -1,13 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const { registerRoute } = workbox.routing;
-const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { registerRoute, NavigationRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate, NetworkOnly } = workbox.strategies;
 const { CacheableResponse } = workbox.cacheableResponse;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute } = workbox.precaching;
 const { setCacheNameDetails } = workbox.core;
 
-const version = "ns32";
+const version = "ns33";
 
 workbox.core.setCacheNameDetails({
   suffix: version
@@ -60,9 +60,35 @@ self.addEventListener("activate", function (event) {
 });
 
 //offline fallback
-const FALLBACK_URL = "/offline.html";
-const urlHandler = CacheFirst;
+const CACHE_NAME = 'offline-html';
+// This assumes /offline.html is a URL for your self-contained
+// (no external images or styles) offline page.
+const FALLBACK_HTML_URL = '/offline.html';
+// Populate the cache with the offline HTML page when the
+// service worker is installed.
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.add(FALLBACK_HTML_URL))
+  );
+});
+
+navigationPreload.enable();
+
+const networkOnly = new NetworkOnly();
+const navigationHandler = async (params) => {
+  try {
+    // Attempt a network request.
+    return await networkOnly.handle(params);
+  } catch (error) {
+    // If it fails, return the cached HTML.
+    return caches.match(FALLBACK_HTML_URL, {
+      cacheName: CACHE_NAME,
+    });
+  }
+};
+
+// Register this strategy to handle all navigations.
 registerRoute(
-  new RegExp("/.*"), ({ event }) => {
-    return urlHandler.handle({ event }).catch(() => caches.match(FALLBACK_URL));
-  });
+  new NavigationRoute(navigationHandler)
+);
