23476d03799cc3d18860830a517c9adae6cb336c
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 20:17:17 +0100

    Trying to sort SW

--- static/sw.js
+++ static/sw.js
@@ -7,10 +7,7 @@ const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute } = workbox.precaching;
 const { setCacheNameDetails } = workbox.core;
 
-const version = "ns10";
-
-let validCacheSet = new Set(Object.values(workbox.core.cacheNames));
-console.log(validCacheSet);
+const version = "ns11";
 
 precacheAndRoute([
   { url: '/index.html', revision: version },
@@ -21,20 +18,12 @@ registerRoute(
   // Cache style resources, i.e. CSS files.
   ({ request }) => request.destination === 'style',
   // Use cache but update in the background.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'css-cache-' + version,
-  })
 );
 
 registerRoute(
   // Cache style resources, i.e. CSS files.
   ({ request }) => request.destination === 'script',
   // Use cache but update in the background.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'js-cache-' + version,
-  })
 );
 
 
@@ -43,8 +32,7 @@ registerRoute(
   ({ request }) => request.destination === 'image',
   // Use the cache if it's available.
   new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'image-cache-' + version,
+
     plugins: [
       new ExpirationPlugin({
         // Cache only 20 images.
