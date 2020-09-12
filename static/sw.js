f0062c229612e480d7fecdcc047f2cf31247d3c0
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 23:43:14 +0100

    Trying to sort SW

--- static/sw.js
+++ static/sw.js
@@ -27,12 +27,6 @@
       });
   }
 
-  function serveOfflineImage(request) {
-    if (request.headers.get('Accept').indexOf('image') !== -1) {
-      return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
-    }
-  }
-
   self.addEventListener('install', function (event) {
     event.waitUntil(updateStaticCache());
   });
