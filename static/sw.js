ea412eb6a5dd4e824688ba6750ecf28c9781e0b4
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 23:17:30 +0100

    Trying to sort SW

--- static/sw.js
+++ static/sw.js
@@ -1,93 +1,129 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+(function () {
+  'use strict';
 
-const { registerRoute, NavigationRoute } = workbox.routing;
-const { CacheFirst, StaleWhileRevalidate, NetworkOnly } = workbox.strategies;
-const { CacheableResponse } = workbox.cacheableResponse;
-const { ExpirationPlugin } = workbox.expiration;
-const { precacheAndRoute } = workbox.precaching;
-const { setCacheNameDetails } = workbox.core;
+  // Update 'version' if you need to refresh the cache
+  var version = 'NS19022020v1::CacheFirstSafe';
+  var offlineUrl = "/offline.html";
 
-const version = "ns36";
-
-workbox.core.setCacheNameDetails({
-  suffix: version
-});
-
-precacheAndRoute([
-  { url: '/index.html', revision: version },
-  { url: '/img/offline.png', revision: version }
-]);
-
-registerRoute(
-  // Cache style resources, i.e. CSS files.
-  ({ request }) => request.destination === 'style',
-  // Use cache but update in the background.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'css-cache-' + version,
-  })
-);
-
-registerRoute(
-  // Cache style resources, i.e. CSS files.
-  ({ request }) => request.destination === 'script',
-  // Use cache but update in the background.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'js-cache-' + version,
-  })
-);
-
-
-registerRoute(
-  // Cache image files.
-  ({ request }) => request.destination === 'image',
-  // Use the cache if it's available.
-  new CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'image-cache-' + version,
-  })
-);
-
-//clear invalid caches
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((keys) => keys.filter((key) => !key.endsWith(version)))
-      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
-  );
-});
-
-//offline fallback
-const CACHE_NAME = 'offline-html-' + version;
-// This assumes /offline.html is a URL for your self-contained
-// (no external images or styles) offline page.
-const FALLBACK_HTML_URL = '/offline.html';
-// Populate the cache with the offline HTML page when the
-// service worker is installed.
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.add(FALLBACK_HTML_URL))
-  );
-});
-
-
-const networkOnly = new NetworkOnly();
-const navigationHandler = async (params) => {
-  try {
-    // Attempt a network request.
-    return await networkOnly.handle(params);
-  } catch (error) {
-    // If it fails, return the cached HTML.
-    return caches.match(FALLBACK_HTML_URL, {
-      cacheName: CACHE_NAME,
-    });
+  // Store core files in a cache (including a page to display when offline)
+  function updateStaticCache() {
+    return caches.open(version)
+      .then(function (cache) {
+        return cache.addAll([
+          offlineUrl,
+          '/'
+        ]);
+      });
   }
-};
 
-// Register this strategy to handle all navigations.
-registerRoute(
-  new NavigationRoute(navigationHandler)
-);
+  function addToCache(request, response) {
+    if (!response.ok && response.type !== 'opaque')
+      return;
+
+    var copy = response.clone();
+    caches.open(version)
+      .then(function (cache) {
+        cache.put(request, copy);
+      });
+  }
+
+  function serveOfflineImage(request) {
+    if (request.headers.get('Accept').indexOf('image') !== -1) {
+      return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
+    }
+  }
+
+  self.addEventListener('install', function (event) {
+    event.waitUntil(updateStaticCache());
+  });
+
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(
+      caches.keys()
+        .then(function (keys) {
+          // Remove caches whose name is no longer valid
+          return Promise.all(keys
+            .filter(function (key) {
+              return key.indexOf(version) !== 0;
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+          );
+        })
+    );
+  });
+
+  self.addEventListener('fetch', function (event) {
+    var request = event.request;
+
+    // Always fetch non-GET requests from the network
+    if (request.method !== 'GET' || request.url.match(/\/browserLink/ig)) {
+      event.respondWith(
+        fetch(request)
+          .catch(function () {
+            return caches.match(offlineUrl);
+          })
+      );
+      return;
+    }
+
+    // For HTML requests, try the network first, fall back to the cache, finally the offline page
+    if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+      event.respondWith(
+        fetch(request)
+          .then(function (response) {
+            // Stash a copy of this page in the cache
+            addToCache(request, response);
+            return response;
+          })
+          .catch(function () {
+            return caches.match(request)
+              .then(function (response) {
+                return response || caches.match(offlineUrl);
+              });
+          })
+      );
+      return;
+    }
+
+    // cache first for fingerprinted resources
+    if (request.url.match(/(\?|&)v=/ig)) {
+      event.respondWith(
+        caches.match(request)
+          .then(function (response) {
+            return response || fetch(request)
+              .then(function (response) {
+                addToCache(request, response);
+                return response || serveOfflineImage(request);
+              })
+              .catch(function () {
+                return serveOfflineImage(request);
+              });
+          })
+      );
+
+      return;
+    }
+
+    // network first for non-fingerprinted resources
+    event.respondWith(
+      fetch(request)
+        .then(function (response) {
+          // Stash a copy of this page in the cache
+          addToCache(request, response);
+          return response;
+        })
+        .catch(function () {
+          return caches.match(request)
+            .then(function (response) {
+              return response || serveOfflineImage(request);
+            })
+            .catch(function () {
+              return serveOfflineImage(request);
+            });
+        })
+    );
+  });
+
+})();
