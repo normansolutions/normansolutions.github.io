e6fc4d3d772e02195f4e001d8077a8b56fd6e1d0
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 16:18:43 +0100

    Trying to sort SW

--- docs/sw.js
+++ docs/sw.js
@@ -1,18 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
-const { StaleWhileRevalidate } = workbox.strategies;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { CacheableResponse } = workbox.cacheableResponse;
-
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [0, 200] })
-    ],
-  })
-);
 
 registerRoute(
   // Cache style resources, i.e. CSS files.
@@ -21,5 +11,23 @@ registerRoute(
   new StaleWhileRevalidate({
     // Use a custom cache name.
     cacheName: 'css-cache',
+  })
+);
+
+registerRoute(
+  // Cache image files.
+  ({ request }) => request.destination === 'image',
+  // Use the cache if it's available.
+  new CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
   })
 );
