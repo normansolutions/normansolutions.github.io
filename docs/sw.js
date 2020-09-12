b1641babe352dc93a0aab6cc0310c41a67f31a6d
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-12 20:26:45 +0100

    Trying to sort SW

--- docs/sw.js
+++ docs/sw.js
@@ -7,7 +7,7 @@ const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute } = workbox.precaching;
 const { setCacheNameDetails } = workbox.core;
 
-const version = "ns11";
+const version = "ns13";
 
 precacheAndRoute([
   { url: '/index.html', revision: version },
@@ -18,12 +18,20 @@ registerRoute(
   // Cache style resources, i.e. CSS files.
   ({ request }) => request.destination === 'style',
   // Use cache but update in the background.
+  new CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'css-cache-' + version,
+  })
 );
 
 registerRoute(
   // Cache style resources, i.e. CSS files.
   ({ request }) => request.destination === 'script',
   // Use cache but update in the background.
+  new CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'js-cache-' + version,
+  })
 );
 
 
@@ -32,7 +40,8 @@ registerRoute(
   ({ request }) => request.destination === 'image',
   // Use the cache if it's available.
   new CacheFirst({
-
+    // Use a custom cache name.
+    cacheName: 'image-cache-' + version,
     plugins: [
       new ExpirationPlugin({
         // Cache only 20 images.
@@ -52,7 +61,7 @@ self.addEventListener("activate", function (event) {
       return Promise.all(
         cacheNames
           .filter(function (cacheName) {
-            return !validCacheSet.has(cacheName);
+            return !validCacheSet.has(version);
           })
           .map(function (cacheName) {
             console.log("deleting cache", cacheName);
