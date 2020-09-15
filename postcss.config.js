c9cabdbe40e030e60b6071461fb1dd7958ce4099
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-15 13:32:28 +0100

    Added Prettier

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,24 @@
 module.exports = {
-    plugins: {
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                './layouts/**/*.html',
-                './assets/*.js',
-            ],
-            whitelist: [
-                'highlight',
-                'language-bash',
-                'pre',
-                'video',
-                'code',
-                'content',
-                'h3',
-                'h4',
-                'ul',
-                'li',
-                'iframe',
-                'img'
-            ],
-            whitelistPatterns: [/fancybox/],
-
-        },
-        autoprefixer: {},
-        cssnano: { preset: 'default' }
-    }
+  plugins: {
+    "@fullhuman/postcss-purgecss": {
+      content: ["./layouts/**/*.html", "./assets/*.js"],
+      whitelist: [
+        "highlight",
+        "language-bash",
+        "pre",
+        "video",
+        "code",
+        "content",
+        "h3",
+        "h4",
+        "ul",
+        "li",
+        "iframe",
+        "img"
+      ],
+      whitelistPatterns: [/fancybox/]
+    },
+    autoprefixer: {},
+    cssnano: { preset: "default" }
+  }
 };
