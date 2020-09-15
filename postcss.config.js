422465a8b466573e8000fd279abcc7a59a9b1b7c
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-15 16:39:43 +0100

    Resolved Prettier Errors Stopping Hugo Displaying

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ["./layouts/**/*.html", "./assets/*.js"],
@@ -23,33 +22,3 @@ module.exports = {
     cssnano: { preset: "default" }
   }
 };
-=======
-    plugins: {
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                './layouts/**/*.html',
-                './assets/*.js',
-            ],
-            whitelist: [
-                'highlight',
-                'language-bash',
-                'pre',
-                'video',
-                'code',
-                'content',
-                'h3',
-                'h4',
-                'ul',
-                'li',
-                'iframe',
-                'img'
-            ],
-            whitelistPatterns: [
-            ],
-
-        },
-        autoprefixer: {},
-        cssnano: { preset: 'default' }
-    }
-};
->>>>>>> parent of aab7f71... Added fancybox and resolved local bundling
