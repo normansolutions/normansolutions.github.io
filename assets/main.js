4951fbba99bf99b16ac034b63aa2c2c7e270f152
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-16 10:26:39 +0100

    Added quicklinks

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,10 @@
 var listofgigstocome = document.querySelectorAll("ul>li.ns-notpassed");
 var nextgig = listofgigstocome[listofgigstocome.length - 1];
 nextgig.classList.add("ns-nextgig");
+
+window.addEventListener("load", function () {
+  quicklink({
+    origins: !0,
+    priority: !0
+  });
+});
