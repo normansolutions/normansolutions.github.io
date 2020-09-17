49907c7b25502d7542e0d24f7d17c68ca8e5f708
Clive Norman <cjn@hazlegrove.co.uk>
2020-09-17 09:32:21 +0100

    Test Firefox Font Issue

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS064::CacheFirstSafe";
+  var version = "NS065::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
