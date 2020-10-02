d7e5a63992f848c456a9f9b52073065a549471c3
Clive Norman <cjn@hazlegrove.co.uk>
2020-10-02 20:54:51 +0100

    Changed SW

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS204::CacheFirstSafe";
+  var version = "NS210::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
