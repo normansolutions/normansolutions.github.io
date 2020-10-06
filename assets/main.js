18bba8d5f985f7abc1d2c9ffbbc1da782b906aed
Clive Norman <cjn@hazlegrove.co.uk>
2020-10-06 11:03:39 +0100

    Hidden Summary On All Posts List

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,3 @@
-var listofgigstocome = document.querySelectorAll("ul>li.ns-notpassed");
-var nextgig = listofgigstocome[listofgigstocome.length - 1];
-if (nextgig) {
-  nextgig.classList.add("ns-nextgig");
-}
-
 window.addEventListener("load", () => {
   quicklink.listen({
     origins: !0,
@@ -93,51 +87,18 @@ window.addEventListener("load", function () {
   init();
 });
 
-function decodeEmail(encodedString) {
-  // Holds the final output
-  var email = "";
-
-  // Extract the first 2 letters
-  var keyInHex = encodedString.substr(0, 2);
-
-  // Convert the hex-encoded key into decimal
-  var key = parseInt(keyInHex, 16);
-
-  // Loop through the remaining encoded characters in steps of 2
-  for (var n = 2; n < encodedString.length; n += 2) {
-    // Get the next pair of characters
-    var charInHex = encodedString.substr(n, 2);
-
-    // Convert hex to decimal
-    var char = parseInt(charInHex, 16);
-
-    // XOR the character with the key to get the original character
-    var output = char ^ key;
-
-    // Append the decoded character to the output
-    email += String.fromCharCode(output);
+window.addEventListener("load", () => {
+  const params = new URLSearchParams(window.location.search);
+  var postsList = params.get("post");
+  if (postsList == "allposts") {
+    var divsToHide = document.getElementsByClassName("ns-post-summary"); //divsToHide is an array
+    for (var i = 0; i < divsToHide.length; i++) {
+      divsToHide[i].style.visibility = "hidden"; // or
+      divsToHide[i].style.display = "none"; // depending on what you're doing
+    }
+    var appendToPageLink = document.getElementsByClassName("page-link");
+    for (var i = 0; i < appendToPageLink.length; i++) {
+      appendToPageLink[i].href = appendToPageLink[i].href + "?post=allposts";
+    }
   }
-  return email;
-}
-
-// Find all the elements on the page that use class="eml-protected"
-var allElements = document.getElementsByClassName("eml-protected");
-
-// Loop through all the elements, and update them
-for (var i = 0; i < allElements.length; i++) {
-  updateAnchor(allElements[i]);
-}
-
-function updateAnchor(el) {
-  // fetch the hex-encoded string
-  var encoded = el.innerHTML;
-
-  // decode the email, using the decodeEmail() function from before
-  var decoded = decodeEmail(encoded);
-
-  // Replace the text (displayed) content
-  el.textContent = decoded;
-
-  // Set the link to be a "mailto:" link
-  el.href = "mailto:" + decoded;
-}
+});
