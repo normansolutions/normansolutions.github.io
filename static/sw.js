2ba0eaaad88421e844c426f5a2c869b9e88e59f1
Clive Norman <cjn@hazlegrove.co.uk>
2020-10-08 21:36:04 +0100

    Trying to sort FF font

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS294::CacheFirstSafe";
+  var version = "NS297::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
