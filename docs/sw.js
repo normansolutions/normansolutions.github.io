6f79fc5e5e58639ca1c7fc1db493cc8578017138
Clive Norman <cjn@hazlegrove.co.uk>
2020-10-09 09:53:23 +0100

    Sorted icons

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS313::CacheFirstSafe";
+  var version = "NS314::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
