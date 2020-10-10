48efc3241496fcbac0c24fdf117e5d6c93208317
Clive Norman <cjn@hazlegrove.co.uk>
2020-10-10 21:57:57 +0100

    Added Post

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS338::CacheFirstSafe";
+  var version = "NS339::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
