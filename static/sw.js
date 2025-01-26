214101508e4538051356f1796fb717023a0e53a1
NormanSolutions <cjnuk@outlook.com>
2025-01-26 16:34:35 +0000

    New Post

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Update 'version' if you need to refresh the cache
-  var version = "NS26012025V5::CacheFirstSafe";
+  var version = "NS26012025V6::CacheFirstSafe";
   var offlineUrl = "/offline.html";
 
   // Store core files in a cache (including a page to display when offline)
